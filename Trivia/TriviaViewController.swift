62ef73963bed039b9820abf08275049cacbe1c1e
Monica Nguyen-Hoang <nguyenhl@dickinson.edu>
2024-03-13 15:28:55 -0400

    Connect UI elements

--- Trivia/TriviaViewController.swift
+++ Trivia/TriviaViewController.swift
@@ -9,21 +9,25 @@ import UIKit
 
 class TriviaViewController: UIViewController {
 
+    @IBOutlet weak var questionWindow: UIView!
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var currentQuestionNumber: UILabel!
+    
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    @IBOutlet weak var answerButton1: UIButton!
+    
+    @IBOutlet weak var answerButton2: UIButton!
+    
+    @IBOutlet weak var answerButton3: UIButton!
+    
+    @IBOutlet weak var answerButton4: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
