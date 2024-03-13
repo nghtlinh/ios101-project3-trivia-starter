2448e72e42782014044802ed0c07c4a350088c7a
Monica Nguyen-Hoang <nguyenhl@dickinson.edu>
2024-03-13 18:47:52 -0400

    Finished pj 3

--- Trivia/TriviaQuestion.swift
+++ Trivia/TriviaQuestion.swift
@@ -9,48 +9,9 @@ import Foundation
 import UIKit
 
 struct TriviaQuestion {
-  let category: String
-  let question: TrviaQuestionCode
-  let correctAnswer: String
-  let incorrectAnswers: [String]
-  let questionNum: Int
-    
+    let questionText: String
+    let choices: [String]
+    let correctAnswerIndex: Int
+    let category: String
 }
 
-enum TrviaQuestionCode {
-  case question1
-  case question2
-  case question3
-  case question4
-  case question5
-    
-  var description: String {
-    switch self {
-    case .question1:
-      return "What year did BigBang debut?"
-    case .question2:
-      return "Who is the leader of BigBang?"
-    case .question3:
-      return "Which member of BigBang is known for his solo hits like Eyes, Nose, Lips?"
-    case .question4:
-      return "Which member has a nickname Lady Smell?"
-    case .question5:
-      return "Which member was selected to go to the moon?"
-    }
-  }
-    
-    func getQuestionData(questionCode: TrviaQuestionCode) -> TriviaQuestion {
-         switch questionCode {
-         case .question1:
-             return TriviaQuestion(category: "Debut Year", question: .question1, correctAnswer: "2006", incorrectAnswers: ["2004", "2008", "2010"], questionNum: 1)
-         case .question2:
-             return TriviaQuestion(category: "Leadership", question: .question2, correctAnswer: "G-Dragon", incorrectAnswers: ["TOP", "Taeyang", "Daesung"], questionNum: 2)
-         case .question3:
-             return TriviaQuestion(category: "Solo Hits", question: .question3, correctAnswer: "Taeyang", incorrectAnswers: ["G-Dragon", "TOP", "Daesung"], questionNum: 3)
-         case .question4:
-             return TriviaQuestion(category: "Nicknames", question: .question4, correctAnswer: "Daesung", incorrectAnswers: ["G-Dragon", "TOP", "Taeyang"], questionNum: 4)
-         case .question5:
-             return TriviaQuestion(category: "Moon Selection", question: .question5, correctAnswer: "TOP", incorrectAnswers: ["G-Dragon", "Taeyang", "Daesung"], questionNum: 5)
-         }
-     }
-}
--- Trivia/TriviaViewController.swift
+++ Trivia/TriviaViewController.swift
@@ -25,24 +25,91 @@ class TriviaViewController: UIViewController {
     
     @IBOutlet weak var answerButton4: UIButton!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let fakeQuestion = TriviaQuestion(category: "Debut Year",
-                                          question: .question1,
-                                          correctAnswer: "2006",
-                                          incorrectAnswers: ["2004", "2008", "2010"],
-                                          questionNum: 1)
-        configure(with: fakeQuestion)
-    }
-    private func configure(with question: TriviaQuestion) {
-            categoryLabel.text = question.category
-        questionText.text = question.question.description
-            currentQuestionNumber.text = "Question:\(question.questionNum)/5"
-            
-            var answers = question.incorrectAnswers
-            answers.append(question.correctAnswer)
-            answers.shuffle()
+    var currentQuestionIndex = 0
+    var questions: [TriviaQuestion] = [
+        TriviaQuestion(questionText: "When did BigBang debut?", choices: ["2006", "2007", "2008", "2009"], correctAnswerIndex: 1, category: "Debut Time"),
+        TriviaQuestion(questionText: "Who is the Lady Smell of BigBang?", choices: ["TOP", "Daesung", "G-Dragon", "Taeyang"], correctAnswerIndex: 2, category: "Funny Meme"),
+        TriviaQuestion(questionText: "Who sang the song Eyes, Nose, Lips?", choices: ["TOP", "Daesung", "G-Dragon", "Taeyang"], correctAnswerIndex: 4, category: "Solo song"),
+        TriviaQuestion(questionText: "Who was offered to go to the moon?", choices: ["TOP", "Daesung", "G-Dragon", "Taeyang"], correctAnswerIndex: 1, category: "Other than music"),
+    ]
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            displayQuestion()
+            let backgroundImage = UIImageView(image: UIImage(named: "bigbang2.jpg"))
+                            backgroundImage.contentMode = .scaleAspectFill
+                            backgroundImage.frame = view.bounds
+                            view.addSubview(backgroundImage)
+                            view.sendSubviewToBack(backgroundImage)
+        }
+    
+    func displayQuestion(){
+        let currentQuestion = questions[currentQuestionIndex]
+        questionText.text = currentQuestion.questionText
+        questionText.font = UIFont.boldSystemFont(ofSize: 40)
+        currentQuestionNumber.font = UIFont.boldSystemFont(ofSize: 30)
+        currentQuestionNumber.text="Question \(currentQuestionIndex + 1)/\(questions.count)"
+        
+        answerButton1.setTitle(currentQuestion.choices[0], for: .normal)
+        answerButton1.setTitleColor(.black, for: .normal)
+        answerButton2.setTitle(currentQuestion.choices[1], for: .normal)
+        answerButton2.setTitleColor(.black, for: .normal)
+        answerButton3.setTitle(currentQuestion.choices[2], for: .normal)
+        answerButton3.setTitleColor(.black, for: .normal)
+        answerButton4.setTitle(currentQuestion.choices[3], for: .normal)
+        answerButton4.setTitleColor(.black, for: .normal)
     }
         
-
+    @IBAction func didTapButton1(_ sender: UIButton) {
+        handleAnswerTapped(choiceIndex: 0)
+    }
+    
+    @IBAction func didTapButton2(_ sender: UIButton) {
+        handleAnswerTapped(choiceIndex: 1)
+    }
+    
+    @IBAction func didTapButton3(_ sender: UIButton) {
+        handleAnswerTapped(choiceIndex: 2)
+    }
+    
+    @IBAction func didTapButton4(_ sender: UIButton) {
+        handleAnswerTapped(choiceIndex: 3)
+    }
+    
+    func handleAnswerTapped(choiceIndex: Int) {
+            let currentQuestion = questions[currentQuestionIndex]
+            
+            // Check if the selected answer is correct
+            if choiceIndex == currentQuestion.correctAnswerIndex {
+                print("Correct!")
+                // Handle correct answer logic
+            } else {
+                print("Incorrect!")
+                // Handle incorrect answer logic
+            }
+            // Move to the next question
+            currentQuestionIndex += 1
+            if currentQuestionIndex < questions.count {
+                displayQuestion()
+            } else {
+                displaySummary()
+            }
+        }
+    
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [UIColor(red: 0.54, green: 0.88, blue: 0.99, alpha: 1.00).cgColor,
+                                UIColor(red: 0.51, green: 0.81, blue: 0.97, alpha: 1.00).cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    func displaySummary() {
+            let alertController = UIAlertController(title: "Game Done!", 
+                                                    message: "Thanks for playing the game!",
+                                                    preferredStyle: .alert)
+            present(alertController, animated: true, completion: nil)
+        }
 }
